fd4c9b7fa5f92f2ef40b59181a5ece44a756d7a9
ismail <ismail@ismails-MacBook-Pro.local>
2017-06-23 17:25:22 +0300

    default first view is created

--- TemplateTest/ViewController.swift
+++ TemplateTest/ViewController.swift
@@ -10,14 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        imageView.image = UIImage(named: "addwise_logo_v2_40")
+        textLabel.text = "Template"
+        
     }
 
 
